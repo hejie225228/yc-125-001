f09f86168b5568eab4b77d3f3bfd4db6b37157a8
阿斯顿 <2581689551@qq.cmo>
2023-07-25 18:42:44 +0800

    第三次提交

--- src/main/java/com/yc/test/案例一/UserDaoImpl.java
+++ src/main/java/com/yc/test/案例一/UserDaoImpl.java
@@ -1,4 +1,15 @@
 package com.yc.test.案例一;
 
-public class UserDaoImpl implements UserDao {
+import com.yc.test.案例一.dao.UserDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+class UserDaoImpl implements UserDao {
+    public UserDaoImpl(){
+        System.out.println("UserDaoImpl类的构造。。。。");
+    }
+    @Override
+    public void add(String uname) {
+        System.out.println("添加了："+uname);
+    }
 }
---
+++ src/main/java/com/yc/test/案例一/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.yc.test.案例一.dao;
+
+/**
+ * 借口
+ */
+public interface UserDao {
+    public void  add(String uname);
+}
